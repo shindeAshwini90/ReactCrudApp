470f94f6a592d16b79196025be536f04a11e8bc3
Patil Ashwini Tukaram <ashwini.patil@yash.com>
2019-10-01 14:47:02 +0530

    second commit

---
+++ src/tables/UserTable.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Usertable=props=>(
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>UserName</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <button
+                onClick={() => {
+                  props.editRow(user)
+                }}
+                className="button muted-button"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => props.deleteUser(user.id)}
+                className="button muted-button"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+export default Usertable;
